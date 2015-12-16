e856728c86553ab5aa1cd802bcf59132d64dae98
Jasmine Hsu <jasminechsu@gmail.com>
2015-12-16 00:45:31 -0500

    adding a resource page

--- app/Reservation.py
+++ app/Reservation.py
@@ -1,11 +1,12 @@
-from app import Time
+from app import Time,Author
+from google.appengine.ext import ndb
 
 class Reservation(ndb.Model):
-	# def __init__(self, user_name, resource, rsvp_time):
-	# 	self.user_name = user_name
-	# 	self.resource = resource
-	# 	self.rsvp_time = rsvp_time
-	user_name = ndb.StructuredProperty(User)
+    # def __init__(self, user_name, resource, rsvp_time):
+    #   self.user_name = user_name
+    #   self.resource = resource
+    #   self.rsvp_time = rsvp_time
+    user_name = ndb.StructuredProperty(Author)
     resource = ndb.StringProperty(indexed=False)
     created = ndb.DateTimeProperty(auto_now_add=True)
     modified = ndb.DateTimeProperty(auto_now=True)
--- app/Resource.py
+++ app/Resource.py
@@ -1,5 +1,5 @@
 import Time
-
+from google.appengine.ext import ndb
 
 class Tag(ndb.Model):
 	name = ndb.StringProperty()
@@ -11,26 +11,26 @@ class Resource(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
     tags = ndb.StructuredProperty(Tag, repeated=True)
 
-class Resource(ndb.Model):
-	name = ndb.StringProperty()
-	availability = StructuredProperty(Time)
-    tags = ndb.StructuredProperty(Tag, repeated=True)
-    rsvps = ndb.StructuredProperty(Reservation, repeated=True)
+# class Resource(ndb.Model):
+# 	name = ndb.StringProperty()
+# 	availability = StructuredProperty(Time)
+#     tags = ndb.StructuredProperty(Tag, repeated=True)
+#     rsvps = ndb.StructuredProperty(Reservation, repeated=True)
 
 
-	def __init__(self, name, avail, tags):
-		self.name = name
-		self.availability = availability
-		self.tags = tags
-		self.rsvps = [] 
-		self.owner = None
+# 	def __init__(self, name, avail, tags):
+# 		self.name = name
+# 		self.availability = availability
+# 		self.tags = tags
+# 		self.rsvps = [] 
+# 		self.owner = None
 
-	def set_owner(self, owner):
-		self.owner = owner
+	# def set_owner(self, owner):
+	# 	self.owner = owner
 
-	def add_reservation(self, new_time):
-		if self.availability.in_timespan(new_time):
-			self.rsvp.append(new_time)
+	# def add_reservation(self, new_time):
+	# 	if self.availability.in_timespan(new_time):
+	# 		self.rsvp.append(new_time)
 
-	def get_desc_times(self):
-		return sorted(self.rsvps)
+	# def get_desc_times(self):
+	# 	return sorted(self.rsvps)

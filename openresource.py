7949a955f398864c664f2b41c998495b881ff9d8
Jasmine Hsu <jasminechsu@gmail.com>
2015-12-17 15:11:12 -0500

    resource view page and edit page

--- openresource.py
+++ openresource.py
@@ -7,6 +7,7 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+import urlparse
 import cgi
 
 # from app import Author
@@ -34,33 +35,21 @@ def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     """
     return ndb.Key('Guestbook', guestbook_name)
 
-# class Time():
-#     def __init__(self, Date, start, end):
-#         self.Date = None
-#         self.start = None
-#         self.end = None
-
-#     def __repr__(self):
-#         return "[Availability] %s from %s until %s" %(self.Date, self.start, self.end)
-
 class Time(ndb.Model):
     Date = ndb.StringProperty(indexed=False)
     start = ndb.StringProperty(indexed=False)
     end = ndb.StringProperty(indexed=False)
-    # def __repr__(self):
-        # return "[Availability] %s from %s until %s" %(self.Date.toString(), self.start, self.end)
-    def getValue():
-        value = ""
-        dates = self.Date.split("/")
-        value = str(dates[2]) + str(dates[p]) + str(dates[1])
 
 class Tag(ndb.Model):
-    name = ndb.StringProperty()
+    label = ndb.StringProperty()
+
+    def __repr__(self):
+        return "".join(self.label)
 
 class Author(ndb.Model):
     """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
+    identity = ndb.StringProperty(indexed=True)
+    email = ndb.StringProperty(indexed=True)
 
 class Reservation(ndb.Model):
     user_name = ndb.StructuredProperty(Author)
@@ -75,8 +64,7 @@ class Resource(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
     tags = ndb.StructuredProperty(Tag, repeated=True)
     availability = ndb.StructuredProperty(Time)
-    last_rsvp = ndb.StringProperty(indexed=True))
-
+    last_rsvp = ndb.StringProperty(indexed=True)
 
 class Greeting(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
@@ -84,6 +72,13 @@ class Greeting(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+def parseTimeToValue(Date, start, end):
+        value =""
+        dates = Date.split("/")
+        value = str(dates[2]) + str(dates[0]) + str(dates[1])
+        value += str("".join(start.split(":")))
+        value += str("".join(end.split(":")))
+        return value
 
 class MainPage(webapp2.RequestHandler):
 
@@ -94,12 +89,6 @@ class MainPage(webapp2.RequestHandler):
             ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
-
-        resource_query = Resource.query()
-        resources = resource_query.fetch(10).order(-Resource.last_rsvp)
-
-        print "resources?"
-
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
@@ -108,14 +97,26 @@ class MainPage(webapp2.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
+
+        resource_query_all = Resource.query().order(Resource.last_rsvp)
+        resources_all = resource_query_all.fetch()
+
+        resource_query_user = Resource.query(
+            Resource.creator == Author(
+                identity = user.user_id(),
+                email=user.email())).order(Resource.last_rsvp)
+            
+        resources_user = resource_query_user.fetch()
+
+
         template_values = {
             'user': user,
             'greetings': greetings,
             'guestbook_name': urllib.quote_plus(guestbook_name),
             'url': url,
             'url_linktext': url_linktext,
-            'resources':resources,
-            'resources_size':len(resources),
+            'resources_all':resources_all,
+            'resources_user':resources_user,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -164,33 +165,49 @@ class AddResourcePage(webapp2.RequestHandler):
 
         if users.get_current_user():
             resource.creator = Author(
-                identity = users.get_current_user().user_id(),
-                email=users.get_current_user().email())
+                identity = user.user_id(),
+                email=user.email())
 
         resource.name = cgi.escape(self.request.get('resourceName'))
 
+        res_date = cgi.escape(self.request.get('date'))
+        res_start = cgi.escape(self.request.get('timeStart'))
+        res_end = cgi.escape(self.request.get('timeEnd'))
+
         resource.availability = Time(
-            Date = cgi.escape(self.request.get('date')),
-            start = cgi.escape(self.request.get('timeStart')),
-            end = cgi.escape(self.request.get('timeEnd')))
+            Date = res_date,
+            start = res_start,
+            end = res_end)
+
+        resource.last_rsvp = parseTimeToValue(
+            res_date, res_start, res_end)
+
+        res_tags = cgi.escape(self.request.get('tags')).split(",")
+
+        for tag_str in res_tags:
+            tag = Tag(label = tag_str)
+            resource.tags.append(tag)
 
         resource.put()
 
 
-        # greeting.content = self.request.get('content')
-        # greeting.put()
+        resource_query_all = Resource.query().order(-Resource.last_rsvp)
+        resources_all = resource_query_all.fetch()
 
-        class test():
-            inside = "blah"
+        resource_query_user = Resource.query(
+            Resource.creator == Author(
+                identity = user.user_id(),
+                email=user.email()))
+            
+        resources_user = resource_query_user.fetch()
 
-        a = test()
 
         template_values = {
-            'resource_object': resource,
-            'test': a,
             'user': user,
             'url': url,
             'url_linktext': url_linktext,
+            'resources_all':resources_all,
+            'resources_user':resources_user,
         }
 
 
@@ -223,8 +240,38 @@ class Guestbook(webapp2.RequestHandler):
         self.redirect('/?' + urllib.urlencode(query_params))
 
 
+class ViewResourcePage(webapp2.RequestHandler):
+
+    def get(self):
+
+        key = ndb.Key(urlsafe=self.request.get('rkey'))
+        resource = key.get()
+
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+            'resource': resource,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('resource.html')
+        self.response.write(template.render(template_values))
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/add', AddResourcePage),
-    ('/submit', Guestbook)
+    ('/submit', Guestbook),
+    ('/resource', ViewResourcePage)
+
 ], debug=True)
